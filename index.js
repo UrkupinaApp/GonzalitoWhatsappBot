8dc1cad54cdcdf9a8d2092879b1d0a668e7145ee
LucasGiambelluca <lucasdavigiambelluca@gmail.com>
2025-07-15 17:05:47 -0300

    cambios en client

--- index.js
+++ index.js
@@ -21,7 +21,17 @@ const FORMATS_VALIDOS = [
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ]
     }
 });
 
